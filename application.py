d0e8a8fc212cd1dae9fafce1b3128f35e919247f
dlwnsgk529 <junha3rd@gmail.com>
2025-04-05 13:04:40 +0000

    test

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 from flask import Flask
 
 application = Flask(__name__)
+
 @application.route("/")
 def hello():
-    return "Hello!"
-
-if __name__=="__main__":
-    application.run(host='0.0.0.0', debug=True)
+	return "Hello!"
+ 
+if __name__ == "__main__":
+	   application.run(host='0.0.0.0', debug=True)
