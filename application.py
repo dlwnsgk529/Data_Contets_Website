a2be590447d736a8485f00c1f378b80cb4d65647
dlwnsgk529 <junha3rd@gmail.com>
2025-04-05 14:21:58 +0000

    erase all tests

--- application.py
+++ application.py
@@ -9,14 +9,3 @@ def hello():
 if __name__ == "__main__":
 	   application.run(host='0.0.0.0', debug=True)
         
-
-print("a")
-print("b")
-print("c")
-print("d")
-print("e")
-print("f")
-print("g")
-print("hi")
-print("h")
-print("a")
