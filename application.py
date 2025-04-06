92249b69bd69b7f60ccfe2371c84ea5eef546ffa
dlwnsgk529 <junha3rd@gmail.com>
2025-04-06 13:07:25 +0000

    building programming environment

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 application = Flask(__name__)
 
 @application.route("/")
+
 def hello():
-	return "Hello!"
+	return render_template('index.html')
  
 if __name__ == "__main__":
 	   application.run(host='0.0.0.0', debug=True)
-        
