db2adaf42448fb95025f73bd3d0d58c4a5c144df
dlwnsgk529 <junha3rd@gmail.com>
2025-04-06 13:28:30 +0000

    making env

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
-
+import sys
 application = Flask(__name__)
 
 @application.route("/")
-
 def hello():
-	return render_template('index.html')
+	return render_template("hello.html")
  
 if __name__ == "__main__":
 	   application.run(host='0.0.0.0', debug=True)
